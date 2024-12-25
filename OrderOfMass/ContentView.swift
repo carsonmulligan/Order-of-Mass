725f2fe537cedcb18148b4de703dda5467d1fe09
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-25 02:04:17 -0600

    Initial Commit

---
+++ OrderOfMass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrderOfMass
+//
+//  Created by Carson Mulligan on 12/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrderOfMass/OrderOfMassApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderOfMassApp.swift
+//  OrderOfMass
+//
+//  Created by Carson Mulligan on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OrderOfMassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
